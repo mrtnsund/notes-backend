507d57e344805ccc9a2fa1aea345a0645fe8f396
Morten <mrtnsund@gmmail.com>
2020-03-05 09:46:02 +0100

    endret note-backend

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Note = require('./models/note')
+
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
-
-let notes = [
-    { id: 1, content: "HTML is easy", date: "2019-05-30T17:30:31.098Z", important: true }, 
-    { id: 2, content: "Browser can execute only Javascript", date: "2019-05-30T18:39:34.091Z", important: false }, 
-    { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2019-05-30T19:20:14.298Z", important: true }
-]
 
 app.get('/', (req,res) => {
     res.send('<h1>Overskrift</h1>')
 })
 
 app.get('/api/notes', (req,res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes.map(note => note.toJSON()))
+    })
 })
 
 app.get('/api/notes/:id', (request,response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(request.params.id).then(note => {
+        response.json(note.toJSON())
+    })
 })
+
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
@@ -42,14 +38,14 @@ app.post('/api/notes', (request, response) => {
             error: 'content missing'
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-    }
-    notes=notes.concat(note);
-    response.json(note);
+    })
+    note.save().then(savedNote => {
+        response.json(savedNote.toJSON())
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
